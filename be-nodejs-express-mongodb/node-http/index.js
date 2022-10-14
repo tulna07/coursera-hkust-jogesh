92dc25deb5f0825677b64316222ea4accf12b5d1
scoobytux <tu.lna07@gmail.com>
2022-10-14 14:26:43 +0700

    node http example 1

---
+++ be-nodejs-express-mongodb/node-http/index.js
@@ -0,0 +1,15 @@
+const http = require("http");
+
+const hostname = "localhost";
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+  console.log(req.headers);
+  res.statusCode = 200;
+  res.setHeader("Content-Type", "text/html");
+  res.end("<html><div>Hello</div></html>");
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}`);
+});
