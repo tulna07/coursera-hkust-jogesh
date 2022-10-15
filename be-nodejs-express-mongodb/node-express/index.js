08ade1a2d6c0d8cad7a17adf3d0090016fff7157
scoobytux <tu.lna07@gmail.com>
2022-10-15 21:11:55 +0700

    express serve static files

--- be-nodejs-express-mongodb/node-express/index.js
+++ be-nodejs-express-mongodb/node-express/index.js
@@ -1,13 +1,16 @@
 const express = require("express");
 const http = require("http");
+const morgan = require("morgan");
 
 const hostname = "localhost";
 const port = 3000;
 
 const app = express();
+app.use(morgan("dev"));
+
+app.use(express.static(__dirname + "/plublic"));
 
 app.use((req, res, next) => {
-  console.log(req.headers);
   res.statusCode = 200;
   res.setHeader("Content-Type", "text/html");
   res.end("<html><body>This is an Express server</body></html>");
